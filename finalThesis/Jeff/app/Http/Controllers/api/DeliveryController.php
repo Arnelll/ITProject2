5f89b05d09dc861340f87bf841aaef0e2ab349f3
justineraydungan <35917904+justineraydungan@users.noreply.github.com>
2020-02-14 01:41:03 +0800

    Add files via upload

--- finalThesis/Jeff/app/Http/Controllers/api/DeliveryController.php
+++ finalThesis/Jeff/app/Http/Controllers/api/DeliveryController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use DB;
 use App\Delivery;
+use App\DeliveryDetail;
 use App\Product;
 use App\Supplier;
 
@@ -20,6 +21,27 @@ class DeliveryController extends Controller
         $result = Delivery::orderBy('delivery_id','desc')
         ->paginate(10);
         return view('dashboard.view_delivery', compact('result'));
+    }
+
+    public function delivery_profile($id,$delivery_id){
+        $x['id'] = $id;
+        $z['delivery_id'] = $delivery_id;
+
+        $result = Delivery::where('delivery_id','=',$x)
+        ->first();
+
+        $clients = DeliveryDetail::join('supplier', 'supplier.supplier_id', 'delivery_details.supplier_id')
+        ->where('delivery_details.supplier_id', '=', $x)
+        ->first();
+
+        $products = Delivery::join('delivery_details','delivery.delivery_id','delivery_details.delivery_id')
+        ->join('products','delivery_details.product_id','products.product_id')
+        ->where('delivery.delivery_id','=',$z)
+        ->select('products.product_name','products.quantity')
+        ->get();
+        
+        return view('dashboard.delivery_profile', compact('result', 'supplier', 'products'));
+        
     }
 
     public function new_delivery(){
--- finalThesis/Jeff/app/Http/Controllers/api/ProductCheckoutController.php
+++ finalThesis/Jeff/app/Http/Controllers/api/ProductCheckoutController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Checkout;
 
 class ProductCheckoutController extends Controller
 {
@@ -12,12 +13,64 @@ class ProductCheckoutController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index() {        
-        return view('dashboard.product_checkout');
-        
-    }
+     public function index()
+     {
+         $result = Checkout::orderBy('product_checkout.product_checkout_id', 'desc')
+         ->join('job_order', 'job_order.jo_id', 'product_checkout.jo_id')
+         ->join('clients', 'clients.client_id', 'job_order.client_id')
+         ->paginate(10);
+ 
+         return view('dashboard.product_checkout', compact('result'));
+     }
+ 
 
-    public function view_details() {
-        return view('dashboard.view_prodco');
+     public function view_details($id,$product_checkout_id){
+        $x['id'] = $id;
+        $z['product_checkout_id'] = $product_checkout_id;
+
+        $result = Checkout::where('product_checkout_id','=',$x)
+        ->first();
+
+        $jo = Checkout::join('job_order', 'job_order.jo_id', 'product_checkout.jo_id')
+        ->where('product_checkout.jo_id', '=', $x)
+        ->first();
+
+        $clients= Checkout::join('job_order', 'job_order.jo_id', 'product_checkout.jo_id')
+        ->join('clients','clients.client_id','job_order.client_id')
+        ->where('product_checkout.product_checkout_id', '=', $z)
+        ->first();
+
+        $vehicle = Checkout::join('job_order', 'job_order.jo_id', 'product_checkout.jo_id')
+        ->join('vehicles','vehicles.vehicle_id','job_order.vehicle_id')
+        ->where('product_checkout.product_checkout_id', '=', $z)
+        ->first();
+
+        $mechanic = Checkout::join('job_order', 'job_order.jo_id', 'product_checkout.jo_id')
+        ->join('mechanic','mechanic.mechanic_id','job_order.mechanic_id')
+        ->where('product_checkout.product_checkout_id', '=', $z)
+        ->first();
+
+        /*$products= JobOrder::orderBy('job_order.jo_id', 'asc')
+        ->join('products', 'products.product_id', 'job_order.product_id')
+        ->where('job_order.product_id', '=', $x)
+        ->select('products.*')
+        ->paginate(10);*/
+        // d.quantity, d.retail_price, d.retail_price*d.quantity
+
+        $products = Checkout::join('job_order', 'job_order.jo_id', 'product_checkout.jo_id')
+        ->join('job_order_details','job_order.jo_id','job_order_details.jo_id')
+        ->join('products','job_order_details.product_id','products.product_id')
+        ->where('product_checkout.product_checkout_id', '=', $z)
+        ->select('products.product_name','products.quantity','products.retail_price')
+        ->get();
+
+        $products2 = Checkout::join('productcheckout_details','product_checkout.product_checkout_id','productcheckout_details.productcheckout_id')
+        ->join('products','productcheckout_details.product_id','products.product_id')
+        ->where('product_checkout.product_checkout_id','=',$z)
+        ->select('products.product_name','products.quantity','products.retail_price')
+        ->get();
+        
+        return view('dashboard.view_prodco', compact('result','clients','vehicle','mechanic','jo','products','products2'));
+        
     }
 }
